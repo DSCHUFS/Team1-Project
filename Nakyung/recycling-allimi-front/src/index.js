30980de2fb109f74d6c23355a2b044bd3a615670
Nakyung Im <nakyoung99@gmail.com>
2021-02-10 23:53:15 +0900

    add images, pages(main, search), route detail pages

--- Nakyung/recycling-allimi-front/src/index.js
+++ Nakyung/recycling-allimi-front/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//export { default as Detail } from './Detail';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
